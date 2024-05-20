9e9dd3baaaf05659fc8dfbb5691fcbda904b77fe
charlenefialho <fialhocharlene@gmail.com>
2024-05-20 19:59:08 -0300

    feat: created class

---
+++ Models/Lead.cs
@@ -0,0 +1,15 @@
+﻿namespace LeadTech.Models
+{
+    public class Lead
+    {
+        public int IdLead { get; set; }
+        public string NomeLead { get; set; }
+        public string EmailLead { get; set; }
+        public string StatusLead { get; set; }
+        public string DataCriacao { get; set; }
+        public int TbClienteIdCliente { get; set; }
+        public Cliente Cliente { get; set; }
+        public int TbCampanhasIdCampanha { get; set; }
+        public Campanha Campanha { get; set; }
+    }
+}
